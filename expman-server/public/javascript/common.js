66f10e80b913fe35c3d9d582bb83f7e9d7c46023
N-Nagorny <nikita.nagorniy@macnica.com>
2021-02-02 01:08:33 +0300

    Use Bootstrap Table for Expenses

--- expman-server/public/javascript/common.js
+++ expman-server/public/javascript/common.js
@@ -14,3 +14,11 @@ function makePostRequest(host, port, page, body, successCallback) {
     error: errorMessage
   });
 }
+
+function detailFormatter(index, row) {
+  var html = []
+  $.each(row, function (key, value) {
+    html.push('<p><b>' + key + ':</b> ' + value + '</p>')
+  })
+  return html.join('')
+}
