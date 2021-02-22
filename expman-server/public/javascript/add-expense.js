045ecdb4ea5c34a5de5830b5ba4969d8ff3ae03a
N-Nagorny <nikiton-n@mail.ru>
2021-02-22 22:32:18 +0300

    Add form validation to the front-end

--- expman-server/public/javascript/add-expense.js
+++ expman-server/public/javascript/add-expense.js
@@ -1,9 +1,9 @@
 $(function addExpenseBtn() {
   if (!($("#addExpenseBtn").length > 0)) {
-    $('#addExpense').toggleClass("d-none");
+    $('#addExpenseForm').toggleClass("d-none");
   }
   $("#addExpenseBtn").click(function(){
-    $('#addExpense').toggleClass("d-none");
+    $('#addExpenseForm').toggleClass("d-none");
   });
 });
 
@@ -12,25 +12,28 @@ $(function addExpense() {
   var host = window.location.hostname;
   const urlParams = new URLSearchParams(window.location.search);
   const purchaseId = urlParams.get('purchaseId');
-  $("#saveExpenseBtn").click(function(){
-    var expense = {
-      'name': document.getElementById("inputExpenseName").value,
-      'type': document.getElementById("inputExpenseType").value,
-      'is_mandatory': document.getElementById("selectIsMandatory").value == "true",
-      'commentary': document.getElementById("inputCommentary").value,
-      'is_single_time': document.getElementById("selectIsSingleTime").value == "true",
-      'cost': Number(document.getElementById("inputCost").value)
-    };
-    var quantity = purchaseId ? 1 : document.getElementById("inputQuantity").value;
-    for (var i = 0; i < quantity - 1; ++i) {
-      makePostRequest(host, port, '/api/expenses', expense, function() {});
-    }
-    makePostRequest(host, port, '/api/expenses', expense, function() {
-      if (purchaseId)
-        makeDeleteRequest(host, port, '/api/purchases', purchaseId, () => window.location.href = '/purchases');
-      else
-        window.location.href = "/expenses";
+  $("#saveExpenseBtn").click(() => {
+    $("#addExpenseForm").validate({
+      submitHandler: () => {
+        var expense = {
+          'name': document.getElementById("inputExpenseName").value,
+          'type': document.getElementById("inputExpenseType").value,
+          'is_mandatory': document.getElementById("selectIsMandatory").value == "true",
+          'commentary': document.getElementById("inputCommentary").value,
+          'is_single_time': document.getElementById("selectIsSingleTime").value == "true",
+          'cost': Number(document.getElementById("inputCost").value)
+        };
+        var quantity = purchaseId ? 1 : document.getElementById("inputQuantity").value;
+        for (var i = 0; i < quantity - 1; ++i) {
+          makePostRequest(host, port, '/api/expenses', expense, function() {});
+        }
+        makePostRequest(host, port, '/api/expenses', expense, function() {
+          if (purchaseId)
+            makeDeleteRequest(host, port, '/api/purchases', purchaseId, () => window.location.href = '/purchases');
+          else
+            window.location.href = "/expenses";
+        });
+      }
     });
   });
 });
-
--- expman-server/public/javascript/add-purchase.js
+++ expman-server/public/javascript/add-purchase.js
@@ -1,22 +1,25 @@
 $(function addPurchaseBtn() {
   $("#addPurchaseBtn").click(function(){
-    $('#addPurchase').toggleClass("d-none");
+    $('#addPurchaseForm').toggleClass("d-none");
   });
 });
-
 
 $(function addPurchase() {
   var port = window.location.port;
   var host = window.location.hostname;
   $("#savePurchaseBtn").click(function(){
-    var purchase = {
-      'name': document.getElementById("inputPurchaseName").value,
-      'type': document.getElementById("inputPurchaseType").value,
-      'is_mandatory': document.getElementById("selectIsMandatory").value == "true",
-      'is_single_time': document.getElementById("selectIsSingleTime").value == "true"
-    };
-    makePostRequest(host, port, '/api/purchases', purchase, function() {
-      window.location.href = "/purchases";
+    $("#addPurchaseForm").validate({
+      submitHandler: () => {
+        var purchase = {
+          'name': document.getElementById("inputPurchaseName").value,
+          'type': document.getElementById("inputPurchaseType").value,
+          'is_mandatory': document.getElementById("selectIsMandatory").value == "true",
+          'is_single_time': document.getElementById("selectIsSingleTime").value == "true"
+        };
+        makePostRequest(host, port, '/api/purchases', purchase, function() {
+          window.location.href = "/purchases";
+        });
+      }
     });
   });
 });
